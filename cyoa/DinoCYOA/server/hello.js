524e5fff1550c5c4b0f08b5be1be578e5eff1e7b
JoelMacDonald83 <joel@cfleket.org>
2026-08-19 13:20:13 -0400

    static server

--- cyoa/DinoCYOA/server/hello.js
+++ cyoa/DinoCYOA/server/hello.js
@@ -1,2 +1,7 @@
-console.log('JavaScript, with no browser attached.')
-console.log('2 + 2 =', 2 + 2)
+import { fileURLToPath} from 'url'
+import {dirname, join} from 'path'
+
+const myPath = fileURLToPath(import.meta.url)
+console.log(myPath) // this will be the whole directory minus the fil:///
+console.log(dirname(myPath)) // this will be myPath minus the file name 
+console.log(join(dirname(myPath), 'test-page.html'))
--- cyoa/DinoCYOA/server/server.js
+++ cyoa/DinoCYOA/server/server.js
@@ -1,22 +1,46 @@
 import { createServer } from "http";
-import { readFileSync } from "fs";
+import { readFile } from "fs/promises";
+import {fileURLToPath} from 'url'
+import { dirname, join, sep, extname } from 'path'
 
-const server = createServer((req, res) => {
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const MIME_TYPES = {
+  ".html": "text/html; charset=utf-8",
+  ".css":  "text/css; charset=utf-8",
+  ".js":   "text/javascript; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".png":  "image/png",
+  ".jpg":  "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".svg":  "image/svg+xml",
+};
+const server = createServer(async (req, res) => {
   console.log("Knock", req.method, req.url);
-  if (req.url === "/") {
-    try {
-      const content = readFileSync("test-page.html", 'utf8');
-      res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  const urlPath = req.url === '/' ? '/index.html' : req.url
+  const requestedPath = join(__dirname, "public", urlPath);
+  console.log("wants:", requestedPath);
 
-      res.end(content);
-    } catch {
-      res.statusCode = 500;
-      res.end(`Something went wrong server side`);
-    }
+  if (!requestedPath.startsWith(join(__dirname, "public") + sep)) {
+    res.statusCode = 403;
+    res.end("Forbidden");
     return;
   }
-  res.statusCode = 404;
-  res.end("File not found");
+
+  try {
+    const content = await readFile(requestedPath);
+    const type = MIME_TYPES[extname(requestedPath)] ?? "application/octet-stream";
+    res.setHeader('Content-Type', type);
+    res.end(content);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      res.statusCode = 404;
+      res.end("File not found");
+    } else {
+      res.statusCode = 500;
+      res.end("Something went wrong server side");
+    }
+  }
 });
 
 server.listen(3000, () => {
